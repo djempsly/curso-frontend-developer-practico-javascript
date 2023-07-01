5a759113c61a0d425164e93daaa16e79d8bc6e8a
Jeffry Almonte <djempsly@MacBook-Pro-de-Jeffry.local>
2023-06-30 23:53:12 -0400

    haciendo los cambios

--- main.js
+++ main.js
@@ -1,58 +1,68 @@
 const navBar = document.querySelector('.navbar-email')//
 const menuImagen = document.querySelector('.menu')//
 const carroCompra  = document.querySelector('.navbar-shopping-cart')//
-const asideArticulo = document.querySelector('.product-detail')
+const shopingCartContainer = document.querySelector('#shopingCardContainer')
 const deskMenu = document.querySelector('.desktop-menu')
 const menuMobile = document.querySelector('.mobile-menu')
 const containerCard = document.querySelector('.cards-container')
+const productDetail = document.querySelector('#productDetail')
+const productDetailClose = document.querySelector('.product-detail-close')
 
 
 navBar.addEventListener('click', menuDeskTop)
 menuImagen.addEventListener('click', menuMobile1)
 carroCompra.addEventListener('click', showArticuloCarro )
+productDetailClose.addEventListener('click', closeProductDetail)
 
 
 function menuDeskTop(){
-     const isDeskMenuOpen = asideArticulo.classList.contains('inactive')
-
+     const isDeskMenuOpen = shopingCartContainer.classList.contains('inactive')
      if (!isDeskMenuOpen) {
-          asideArticulo.classList.add('inactive')
-          
+          shopingCartContainer.classList.add('inactive')   
      }
      deskMenu.classList.toggle('inactive') // menu pequeño
 }
 
 
 function showArticuloCarro (){
-    const isAsideArticuloOpen = menuMobile.classList.contains('inactive')
-    const isAsideArticuloClosed = deskMenu.classList.contains('inactive')
+    const isshopingCartContainerOpen = menuMobile.classList.contains('inactive')
+    const isshopingCartContainerClosed = deskMenu.classList.contains('inactive')
 
-    if (!isAsideArticuloOpen) {
+    if (!isshopingCartContainerOpen) {
      menuMobile.classList.add('inactive')
      
     }
 
-    if (!isAsideArticuloClosed) {
+    if (!isshopingCartContainerClosed) {
      deskMenu.classList.add('inactive')
      
     }
-     asideArticulo.classList.toggle('inactive') // Carro de 
+     shopingCartContainer.classList.toggle('inactive') // Carro de 
      
    }
 
-
-
 function menuMobile1 (){
 
-     const isMenuMobileOpen = asideArticulo.classList.contains('inactive')
+     const isMenuMobileOpen = shopingCartContainer.classList.contains('inactive')
 
      if (!isMenuMobileOpen) {
-          asideArticulo.classList.add('inactive')
+          shopingCartContainer.classList.add('inactive')
           
      }
    
      menuMobile.classList.toggle('inactive')
     
+}
+
+function openProductDetail (){
+     productDetail.classList.remove('inactive')
+    
+     
+}
+
+function closeProductDetail (){
+     productDetail.classList.add('inactive')
+
 }
 
 //En esta parte está la interacion del DOM desde javascript
@@ -95,6 +105,7 @@ function articulosList (arr){
 
          const productImg = document.createElement('img'); // En esta parte creando en la imagen de los productos.
           productImg.setAttribute('src', product.imagen);
+          productImg.addEventListener('click', openProductDetail)
 
           const productsInfo = document.createElement('div'); //  Aqui creando las informaciones de los productos (precio y nombre)
           productsInfo.classList.add('product-info');
